6747f440197a020a30556383f183d4e34949aab6
Mundial419 <mundial419@gmail.com>
2020-03-20 20:44:14 +0100

    html editor

--- js/script.js
+++ js/script.js
@@ -33,6 +33,67 @@ var startVoice = function(e) {
 	return  recognition.start();
 };
 
+function update()
+{
+	var idoc = document.getElementById('iframe').contentWindow.document;
+
+	idoc.open();
+	idoc.write(editor.getValue());
+	idoc.close();
+}
+
+function setupEditor()
+{
+  window.editor = ace.edit("editor");
+  editor.setTheme("ace/theme/monokai");
+  editor.getSession().setMode("ace/mode/html");
+	
+  editor.setValue(`<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+</head>
+
+<body>
+The content of the document......
+</body>
+
+</html>
+
+
+
+
+`,1);
+ //1 = moves cursor to end
+	/*var customPosition = {
+    row: ,
+    column: 0
+};
+
+var text = "This text has been dinamically inserted";
+
+editor.session.insert(customPosition , text);*/
+  editor.getSession().on('change', function() {
+    update();
+  });
+
+  editor.focus();
+  
+  
+  editor.setOptions({
+    fontSize: "14pt",
+    showLineNumbers: true,
+    showGutter: true,
+    vScrollBarAlwaysVisible:true,
+    enableBasicAutocompletion: false, enableLiveAutocompletion: false });
+
+  editor.setShowPrintMargin(false);
+  editor.setBehavioursEnabled(false);
+}
+
+setupEditor();
+update();
+  
 
 
  
